c14bf4d69d20acf8373243c843782a4366e1a874
Nery Villatoro <neryvilla@gmail.com>
2026-06-30 20:27:26 -0600

    Configurando url de producción

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://brinco-backend.onrender.com/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
