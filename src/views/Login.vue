af2d07a10be752bb944f91f6c5fd4efa3bacb82e
Nery Villatoro <neryvilla@gmail.com>
2026-07-05 12:53:14 -0600

    limpiando la interfaz del login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,8 +11,6 @@
           <div class="avatar-view-master mb-8">
             <div class="relative inline-block">
               <!-- Texto de prueba en rojo -->
-              <p class="text-red-500 font-bold bg-white p-2 z-50 absolute">{{ userAvatar }}</p>
-
               <div
                 class="w-36 h-36 rounded-[2.5rem] bg-gradient-to-br from-slate-50 to-slate-100 border-4 border-white shadow-2xl overflow-hidden flex items-center justify-center transition-all duration-700 transform"
                 :class="{ 'scale-110 shadow-cyan-500/20 ring-8 ring-cyan-50': userAvatar }"
